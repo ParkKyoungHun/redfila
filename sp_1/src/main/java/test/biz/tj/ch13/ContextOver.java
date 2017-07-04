b8229f90bb21b59d4d776b91b00a4ea9660cfbba
heroicrails <heroicrails@gmail.com>
2017-07-04 21:46:53 +0900

    t

--- sp_1/src/main/java/test/biz/tj/ch13/ContextOver.java
+++ sp_1/src/main/java/test/biz/tj/ch13/ContextOver.java
@@ -3,6 +3,12 @@ package test.biz.tj.ch13;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class ContextOver {
+	//father.xml String클래스로 str1;
+	//Son String 클래스로 str1;
+	//Son Exam클래스 target4
+	//target4 setVal1(father의 str1)
+	//target4 setVal2(son의 str1)
+	// 출력하는 예제를 
 	public static void main(String[] args) {
 		GenericXmlApplicationContext father = new GenericXmlApplicationContext();
 		father.load("ch13/father.xml");
@@ -11,11 +17,8 @@ public class ContextOver {
 		son.load("ch13/son.xml");
 		son.setParent(father);
 		son.refresh();
-		Exam target1 = (Exam) son.getBean("target1");
-		Exam target2 = (Exam) son.getBean("target2");
-		String target3 = (String) son.getBean("father2");
-		System.out.println(target1.getVal1());
-		System.out.println(target2.getVal2());
-		System.out.println(target3);
+		Exam e = (Exam)son.getBean("target4");
+		System.out.println(e.getVal1());
+		System.out.println(e.getVal2());
 	}
 }
